8859cc07b59395cdaf6208757ce5e9a9c8ff86fd
JaCoderX <jaco1234h@gmail.com>
2025-03-07 10:43:17 +0200

    return of the fix

--- src/lib/SecureOwnableManager.ts
+++ src/lib/SecureOwnableManager.ts
@@ -148,6 +148,7 @@ export class SecureOwnableManager {
 
       return {
         address: this.address,
+        contractAddress: this.address,
         owner,
         broadcaster,
         recoveryAddress,
