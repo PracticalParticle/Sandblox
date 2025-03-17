e2360f44fde81ea74b1c041eff2d207d60da3f6e
JaCoderX <jaco1234h@gmail.com>
2025-03-17 23:44:13 +0200

    minor fix

--- scripts/copy-to-public.ts
+++ scripts/copy-to-public.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import glob from 'glob'
+import { glob } from 'glob'
 
 interface CopyStats {
   totalFiles: number
